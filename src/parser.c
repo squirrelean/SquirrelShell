e6789640145eadb6b7d245637bb94ded2ffaf4ee
Daniel <danastasio02@icloud.com>
2025-08-09 21:32:35 -0400

    feat: Implement foundational recursive-descent parser
    This commit establishes the core architecture of the parser. It builds an abstract syntax tree for the various command structures.
    
    - Updated error handling in parse_command.
    - Added the parsing heirarchy for sequences, pipelines, and redirections.
    - Corrected the parse_token function to call the top-level function.

--- src/parser.c
+++ src/parser.c
@@ -6,6 +6,9 @@
 #include "command_syntax_tree.h"
 
 // Prototypes.
+ASTNode* parse_sequence(Token *tokens, int token_count, int *token_index);
+ASTNode* parse_pipeline(Token *tokens, int token_count, int *token_index);
+ASTNode* parse_redirect(Token *tokens, int token_count, int *token_index);
 ASTNode* parse_command(Token *tokens, int token_count, int *token_index);
 
 ASTNode* parse_token(Token *tokens, int token_count)
@@ -15,13 +18,16 @@ ASTNode* parse_token(Token *tokens, int token_count)
         return NULL;
     }
 
-    // TODO: Call the highest order parse function.
     int token_index = 0;
-    ASTNode *ast = parse_command(tokens, token_count, &token_index);
+    ASTNode *ast = parse_sequence(tokens, token_count, &token_index);
+    if (!ast)
+    {
+        return NULL;
+    }
 
     if (token_index < token_count)
     {
-        // TODO: write function to free the ast.
+        // TODO: write function free_ast to free the ast.
         return NULL;
     }
 
@@ -29,24 +35,128 @@ ASTNode* parse_token(Token *tokens, int token_count)
 }
 
 
-ASTNode* parse_sequence()
+ASTNode* parse_sequence(Token *tokens, int token_count, int *token_index)
 {
-    // TODO
-    return NULL;
+    ASTNode *node_side = parse_pipeline(tokens, token_count, token_index);
+    if (!node_side)
+    {
+        return NULL;
+    }
+
+    if (tokens[*token_index].type != TOKEN_SEMICOLON)
+    {
+        return node_side;
+    }
+
+    ASTNode *node = malloc(sizeof(ASTNode));
+    if (!node)
+    {
+        fprintf(stderr, "parse_sequence: malloc failure\n");
+        // TODO: free_ast()
+        return NULL;
+    }
+
+    node->type = NODE_SEQUENCE;
+    node->binary.left = node_side;
+
+    // Move past semicolon.
+    (*token_index)++;
+
+    node->binary.right = parse_sequence(tokens, token_count, token_index);
+    
+    return node;
 }
 
 
-ASTNode* parse_pipeline()
+ASTNode* parse_pipeline(Token *tokens, int token_count, int *token_index)
 {
-    // TODO
-    return NULL;
+    ASTNode *node_side = parse_redirect(tokens, token_count, token_index);
+    if (!node_side)
+    {
+        return NULL;
+    }
+
+    if (tokens[*token_index].type != TOKEN_PIPE)
+    {
+        return node_side;
+    }
+
+    ASTNode *node = malloc(sizeof(ASTNode));
+    if (!node)
+    {
+        fprintf(stderr, "parse_pipeline: malloc failure\n");
+        // TODO: free_ast()
+        return NULL;
+    }
+
+    node->type = NODE_PIPELINE;
+    node->binary.left = node_side;
+
+    (*token_index)++;
+
+    node->binary.right = parse_pipeline(tokens, token_count, token_index);
+
+    return node;
 }
 
 
-ASTNode* parse_redirect()
+ASTNode* parse_redirect(Token *tokens, int token_count, int *token_index)
 {
     // TODO
-    return NULL;
+    ASTNode *node_command = parse_command(tokens, token_count, token_index);
+    if (!node_command)
+    {
+        return NULL;
+    }
+
+    ASTNode *current_node = node_command;
+
+    while (*token_index < token_count && (
+        tokens[*token_index].type == TOKEN_INPUT_REDIRECT ||
+        tokens[*token_index].type == TOKEN_OUTPUT_REDIRECT ||
+        tokens[*token_index].type == TOKEN_APPEND_REDIRECT))
+    {
+        ASTNode *node = malloc(sizeof(ASTNode));
+        if (!node)
+        {
+            fprintf(stderr, "parse_redirect: malloc failure\n");
+            // TODO: free_ast()
+            return NULL;
+        }
+
+        node->type = NODE_REDIRECT;
+        node->redirect.child = current_node;
+
+        switch (tokens[*token_index].type)
+        {
+            case TOKEN_INPUT_REDIRECT:
+                node->redirect.redirect_type = REDIRECT_INPUT;
+                break;
+            case TOKEN_OUTPUT_REDIRECT:
+                node->redirect.redirect_type = REDIRECT_OUTPUT;
+                break;
+            case TOKEN_APPEND_REDIRECT:
+                node->redirect.redirect_type = REDIRECT_APPEND;
+                break;
+        }
+
+        // Move past redirection token,
+        (*token_index)++;
+
+        if (tokens[*token_index].type != TOKEN_WORD)
+        {
+            fprintf(stderr, "Invalid redirection usage\n");
+            return NULL;
+        }
+        node->redirect.filename = strdup(tokens[*token_index].value);
+
+        // And move past filename.
+        (*token_index)++;
+
+        current_node = node;
+
+    }
+    return current_node;
 }
 
 
@@ -54,14 +164,15 @@ ASTNode* parse_command(Token *tokens, int token_count, int *token_index)
 {
     if (*token_index >= token_count || tokens[*token_index].type != TOKEN_WORD)
     {
-        printf("Invalid command usage\n");
+        fprintf(stderr, "Invalid command usage\n");
         return NULL;
     }
 
     ASTNode *node = malloc(sizeof(ASTNode));
     if (!node)
     {
-        printf("parse_command: node malloc failure");
+        fprintf(stderr, "parse_command: node malloc failure");
+        // TODO: free_ast()
         return NULL;
     }
 
@@ -77,8 +188,9 @@ ASTNode* parse_command(Token *tokens, int token_count, int *token_index)
     node->command.argv = malloc((word_count + 1) * sizeof(char*));
     if (!node->command.argv)
     {
-        printf("parse_command: command.argv malloc failure");
+        fprintf(stderr, "parse_command: command.argv malloc failure");
         free(node);
+        // TODO: free_ast()
         return NULL;
     }
 
@@ -92,4 +204,10 @@ ASTNode* parse_command(Token *tokens, int token_count, int *token_index)
     *token_index += word_count;
 
     return node;
+}
+
+
+void free_ast()
+{
+    // TODO: Implement a function to free all allocated memory.
 }
