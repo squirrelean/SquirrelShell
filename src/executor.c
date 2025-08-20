d7f6801bd84cf4209beb906b7cf4d75bce9bed0d
Daniel <danastasio02@icloud.com>
2025-08-20 17:48:35 -0400

    feat(execute_command): expand functionality to support cd command

--- src/executor.c
+++ src/executor.c
@@ -1,12 +1,15 @@
 #include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <unistd.h>
 #include <sys/types.h>
 #include <sys/wait.h>
 
-
 #include "command_syntax_tree.h"
+#include "builtins.h"
+
+#define CHANGE_DIRECTORY "cd"
 
 // Prototypes.
 int execute_command(ASTNode *node);
@@ -32,24 +35,35 @@ void execute_ast(ASTNode *node)
 
 int execute_command(ASTNode *node)
 {
-    pid_t pid = fork();
-    if (pid < 0)
+    char *command = node->command.argv[0];
+
+    if (strcmp(command, CHANGE_DIRECTORY) == 0)
     {
-        perror("execute_command: fork error");
-        return 1;
-    }
-    else if (pid == 0)
-    {
-        execvp(node->command.argv[0], node->command.argv);
-        perror("execute_command: execvp error");
-        exit(1);
+        // execute change directory function.
+        change_directory(node->command.argv);
     }
     else
     {
-        int status;
-        waitpid(pid, &status, 0);
-        int status_code = WEXITSTATUS(status);
-        return status_code;
-
+        pid_t pid = fork();
+        if (pid < 0)
+        {
+            perror("execute_command: fork error");
+            return 1;
+        }
+        else if (pid == 0)
+        {
+            execvp(node->command.argv[0], node->command.argv);
+            perror("execute_command: execvp error");
+            exit(1);
+        }
+        else
+        {
+            int status;
+            waitpid(pid, &status, 0);
+            int status_code = WEXITSTATUS(status);
+            return status_code;
+        }
     }
+
+    return 0;
 }
