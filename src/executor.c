28fa670fd83dff5c196633abdbaaa688b6c8d033
Daniel <danastasio02@icloud.com>
2025-08-22 17:50:00 -0400

    feat: add support for sequences, AND, OR, and shell exiting
    - implement execute_sequence() for command chaining.
    - implement execute_logical() for &&, ||.
    - expand execute_command() to support exit command and last command execution status.

--- src/executor.c
+++ src/executor.c
@@ -9,39 +9,118 @@
 #include "command_syntax_tree.h"
 #include "builtins.h"
 
+// Built in command names
 #define CHANGE_DIRECTORY "cd"
+#define EXIT_SHELL "exit"
 
 // Prototypes.
-int execute_command(ASTNode *node);
+int execute_command(ASTNode *node, int last_command_status);
+int execute_sequence(ASTNode *node);
+int execute_logical(ASTNode *node);
 
-void execute_ast(ASTNode *node)
+// Global variables
+static int last_command_status = 0;
+
+int execute_ast(ASTNode *node)
 {
     if (!node)
     {
-        return;
+        return 1;
     }
 
     switch (node->type)
     {
         case NODE_COMMAND:
-            execute_command(node);
+            last_command_status = execute_command(node, last_command_status);
+            break;
+
+        case NODE_SEQUENCE:
+            last_command_status = execute_sequence(node);
+            break;
+
+        case NODE_PIPELINE:
+            break;
+
+        case NODE_REDIRECT:
+            break;
+        
+        case NODE_AND:
+        case NODE_OR:
+            last_command_status = execute_logical(node);
+            break;
+        
+        case NODE_SUBSHELL:
             break;
 
         default:
             fprintf(stderr, "command not found\n");
+            last_command_status = 1;
     }
+
+    return last_command_status;
 }
 
 
-int execute_command(ASTNode *node)
+int execute_sequence(ASTNode *node)
+{
+    execute_ast(node->binary.left);
+    
+    return execute_ast(node->binary.right);
+}
+
+
+int execute_logical(ASTNode *node)
+{
+    int left_command_status = execute_ast(node->binary.left);
+
+    if (node->type == NODE_AND)
+    {
+        if (left_command_status == 0)
+        {
+            return execute_ast(node->binary.right);
+        }
+    }
+    
+    else if (node->type == NODE_OR)
+    {
+        if (left_command_status != 0)
+        {
+            return execute_ast(node->binary.right);
+        }
+    }
+    
+    return left_command_status;
+}
+
+
+int execute_pipeline(ASTNode *node)
+{
+    // TODO
+    return 0;
+}
+
+
+int execute_redirect(ASTNode *node)
+{
+    // TODO
+    return 0;
+}
+
+
+int execute_command(ASTNode *node, int last_command_status)
 {
     char *command = node->command.argv[0];
 
-    if (strcmp(command, CHANGE_DIRECTORY) == 0)
+    if (strcmp(command, EXIT_SHELL) == 0)
     {
-        // execute change directory function.
+        exit_shell(last_command_status);
+    }
+
+    else if (strcmp(command, CHANGE_DIRECTORY) == 0)
+    {
         change_directory(node->command.argv);
     }
+
     else
     {
         pid_t pid = fork();
