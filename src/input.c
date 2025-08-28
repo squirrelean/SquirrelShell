e6b9b7c45c43a5b35c77f5962662bb3a0db76b17
Daniel <danastasio02@icloud.com>
2025-08-28 17:44:20 -0400

    feat: refactor read_line() to use print_prompt() and ensure matching quotes

--- src/input.c
+++ src/input.c
@@ -5,6 +5,10 @@
 #include "input.h"
 #include <unistd.h>
 
+// Prototypes.
+void print_prompt(size_t full_command_size);
+bool ensure_matching_quotes(char *line, ssize_t chars_read, int *quote_count);
+
 char* read_line(void)
 {
     size_t buffer_size = 0;
@@ -14,20 +18,13 @@ char* read_line(void)
     char *full_command = NULL;
     size_t full_command_size = 0;
 
+    int quote_count = 0;
+
     while(true)
     {
-        // Print prompt.
-        if (full_command_size > 0)
-        {
-            printf(">>> ");
-        }
-        else
-        {
-            printf("🐿️ > ");
-        }
+        print_prompt(full_command_size);
 
         chars_read = getline(&line, &buffer_size, stdin);
-
         if (chars_read == -1)
         {
             free(line);
@@ -51,23 +48,27 @@ char* read_line(void)
             multi_line = true;
         }
 
-        // Allocate space for new line of code.
+        if (ensure_matching_quotes(line, chars_read, &quote_count))
+        {
+            multi_line = true;
+        }
+
+        // Allocate additional space for new input.
         size_t new_size = full_command_size + chars_read + 2;
         char *temp = realloc(full_command, new_size);
         if (!temp)
         {
-            perror("realloc failure");
+            perror("SquirrelShell: read_line: realloc failure");
             free(line);
             free(full_command);
             return NULL;
         }
         full_command = temp;
 
-        // Copy the new command.
         memcpy(full_command + full_command_size, line, chars_read);
         full_command_size += chars_read;
 
-        // Separate new command with space char.
+        // Separate new command with a space char.
         if (multi_line)
         {
             full_command[full_command_size] = ' ';
@@ -84,4 +85,31 @@ char* read_line(void)
 
     full_command[full_command_size] = '\0';
     return full_command;
+}
+
+
+void print_prompt(size_t full_command_size)
+{
+    if (!full_command_size)
+    {
+        printf("SquirrelShell 🐿️:: ");
+    }
+    else
+    {
+        printf(">");
+    }
+}
+
+
+bool ensure_matching_quotes(char *line, ssize_t chars_read, int *quote_count)
+{
+    for (int i = 0; i < chars_read; i++)
+    {
+        if (line[i] == '"')
+        {
+            (*quote_count)++;
+        }
+    }
+
+    return (*quote_count)% 2 != 0;
 }
