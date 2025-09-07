c019a9427d8fb2c83125a26ea982eb518e145d7f
Daniel <danastasio02@icloud.com>
2025-09-07 11:20:02 -0400

    feat: Implement memory cleanup before exiting
    - Add exit shell message with status of last command.

--- builtins/exit.c
+++ builtins/exit.c
@@ -1,9 +1,35 @@
 #include <stdlib.h>
+#include "tokenizer.h"
+#include "parser.h"
+#include "allocation_resources.h"
+
+#include <stdio.h>
+
+void free_all_allocated_memory();
 
 void exit_shell(int exit_status)
 {
-    // TODO: free all allocated memory.
+    free_all_allocated_memory();
 
+    printf("SquirrelShell: Exiting shell: Last Executed Command Status: %d\n", exit_status);
     
     exit(exit_status);
+}
+
+void free_all_allocated_memory()
+{
+    if (global_allocated_line)
+    {
+        free(global_allocated_line);
+    }
+
+    if (global_allocated_tokens)
+    {
+        free_tokens(global_allocated_tokens, global_allocated_token_count);
+    }
+
+    if (global_allocated_ast)
+    {
+        free_ast(global_allocated_ast);
+    }
 }
