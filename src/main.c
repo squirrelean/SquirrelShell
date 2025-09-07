bd96d26d8a45c723c41cfe2876f3635b9ee53b04
Daniel <danastasio02@icloud.com>
2025-09-07 11:07:26 -0400

    feat: Implement shell loop functionality
    This commit implements the full functionality for the shells main loop. It now properly retrieves input from the user, tokenizes the input, parses it into an AST, and then executes it.
    
    - Implement logic to free all allocated memory before next loop
    - Implement global variables required for freeing allocated memory in exit.c

--- src/main.c
+++ src/main.c
@@ -5,32 +5,54 @@
 
 #include "input.h"
 #include "tokenizer.h"
+#include "parser.h"
+#include "builtins.h"
+#include "executor.h"
+#include "allocation_resources.h"
+
+char *global_allocated_line = NULL;
+Token *global_allocated_tokens = NULL;
+int global_allocated_token_count = 0;
+ASTNode *global_allocated_ast = NULL;
 
 int main()
 {
     do 
     {
         char *line_read = read_line();
-        // Create dedicated check function for this.
-        if (line_read == NULL)
+        if (!line_read)
         {
-            printf("\n");
-            free(line_read);
-            exit(EXIT_FAILURE);
+            continue;
         }
-
-        if (!strcmp(line_read, "exit"))
+        global_allocated_line = line_read;
+        
+        int token_count = 0;
+        Token *tokens = tokenize(line_read, &token_count);
+        if (!tokens)
         {
             free(line_read);
-            break;
+            continue;
         }
+        global_allocated_tokens = tokens;
+        global_allocated_token_count = token_count;
+    
+        ASTNode *ast_root_node = parse_token(tokens, token_count);
+        if (!ast_root_node)
+        {
+            free(line_read);
+            free_tokens(tokens, token_count);
+            continue;
+        }
+        global_allocated_ast = ast_root_node;
 
-        printf("%s\n", line_read);
+        execute_ast(ast_root_node);
         
         // Free all allocated space.
         free(line_read);
+        free_tokens(tokens, token_count);
+        free_ast(ast_root_node);
     } while (true);
-
+    
 
     return EXIT_SUCCESS;
 }
