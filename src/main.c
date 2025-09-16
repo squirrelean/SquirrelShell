05e14b831f2f3148fbdb1a4a579d058f88419fa6
Daniel <danastasio02@icloud.com>
2025-09-16 11:27:42 -0400

    feat: implement custom welcome message with username

--- src/main.c
+++ src/main.c
@@ -2,6 +2,9 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <unistd.h>
+#include <pwd.h>
+#include <sys/types.h>
 
 #include "input.h"
 #include "tokenizer.h"
@@ -9,14 +12,24 @@
 #include "builtins.h"
 #include "executor.h"
 #include "allocation_resources.h"
+#include "welcome_message.h"
 
 char *global_allocated_line = NULL;
 Token *global_allocated_tokens = NULL;
 int global_allocated_token_count = 0;
 ASTNode *global_allocated_ast = NULL;
 
+const char* get_user_name();
+
 int main()
 {
+    const char *username = get_user_name();
+    const char *welcome_message = "Hello, %s. Welcome to SquirrelShell!";
+    int message_len = strlen(username) + strlen(username);
+    char full_welcome_message[sizeof(char) * message_len];
+    sprintf(full_welcome_message, welcome_message, username);
+    print_welcome_message(full_welcome_message);
+
     do 
     {
         char *line_read = read_line();
@@ -55,4 +68,22 @@ int main()
     
 
     return EXIT_SUCCESS;
+}
+
+
+const char* get_user_name()
+{
+    char *username;
+    uid_t uid = getuid();
+    struct passwd *userdata = getpwuid(uid);
+    if (userdata)
+    {
+        username = userdata->pw_name;
+    }
+    else
+    {
+        username = "Anonomous user";
+    }
+
+    return username;
 }
