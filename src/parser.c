5351e8db27d8d7331dd36d0816274ed9b138fda1
squirrelean <danastasio02@icloud.com>
2026-01-09 10:42:21 -0500

    Refactor: remove unnecessary error messages

--- src/parser.c
+++ src/parser.c
@@ -17,18 +17,15 @@ void free_ast(ASTNode *node);
 
 ASTNode *parse_token(Token *tokens, int token_count)
 {
-    if (token_count == 0) {
-        fprintf(stderr, "No tokens to parse.\n");
+    if (token_count == 0)
         return NULL;
-    }
 
     int token_index = 0;
     ASTNode *ast = parse_sequence(tokens, token_count, &token_index);
     if (!ast) {
-        printf("failed to parse sequence.\n");
+        printf("SquirrelShell: syntax error\n");
         return NULL;
     }
-
     if (token_index < token_count && tokens[token_index].type != TOKEN_EOF) {
         printf("Unexpected tokens after parsing index: %d count: %d\n", token_index, token_count);
         free_ast(ast);
@@ -41,10 +38,8 @@ ASTNode *parse_token(Token *tokens, int token_count)
 ASTNode *parse_sequence(Token *tokens, int token_count, int *token_index)
 {
     ASTNode *node_left = parse_logical(tokens, token_count, token_index);
-    if (!node_left) {
-        fprintf(stderr, "parse_sequence: parse_logical failure\n");
+    if (!node_left)
         return NULL;
-    }
 
     while (*token_index < token_count && tokens[*token_index].type == TOKEN_SEMICOLON) {
         (*token_index)++;
@@ -55,10 +50,8 @@ ASTNode *parse_sequence(Token *tokens, int token_count, int *token_index)
         }
 
         ASTNode *node = malloc(sizeof(ASTNode));
-        if (!node) {
-            fprintf(stderr, "parse_sequence: malloc failure\n");
+        if (!node)
             return NULL;
-        }
 
         node->type = NODE_SEQUENCE;
         node->binary.left = node_left;
@@ -73,18 +66,14 @@ ASTNode *parse_sequence(Token *tokens, int token_count, int *token_index)
 ASTNode *parse_logical(Token *tokens, int token_count, int *token_index)
 {
     ASTNode *node_left = parse_pipeline(tokens, token_count, token_index);
-    if (!node_left) {
-        fprintf(stderr, "parse_logical: parse_pipeline failure\n");
+    if (!node_left)
         return NULL;
-    }
 
     while (*token_index < token_count &&
            (tokens[*token_index].type == TOKEN_AND || tokens[*token_index].type == TOKEN_OR)) {
         ASTNode *node = malloc(sizeof(ASTNode));
-        if (!node) {
-            fprintf(stderr, "parse_logical: malloc failure");
+        if (!node)
             return NULL;
-        }
 
         if (tokens[*token_index].type == TOKEN_AND) {
             node->type = NODE_AND;
@@ -110,10 +99,8 @@ ASTNode *parse_logical(Token *tokens, int token_count, int *token_index)
 ASTNode *parse_pipeline(Token *tokens, int token_count, int *token_index)
 {
     ASTNode *node_left = parse_redirect(tokens, token_count, token_index);
-    if (!node_left) {
-        fprintf(stderr, "parse_pipeline: parse_redirect failure\n");
+    if (!node_left)
         return NULL;
-    }
 
     while (*token_index < token_count && tokens[*token_index].type == TOKEN_PIPE) {
         (*token_index)++;
@@ -123,10 +110,8 @@ ASTNode *parse_pipeline(Token *tokens, int token_count, int *token_index)
         }
 
         ASTNode *node = malloc(sizeof(ASTNode));
-        if (!node) {
-            fprintf(stderr, "parse_pipeline: malloc failure\n");
+        if (!node)
             return NULL;
-        }
 
         node->type = NODE_PIPELINE;
         node->binary.left = node_left;
@@ -140,10 +125,8 @@ ASTNode *parse_pipeline(Token *tokens, int token_count, int *token_index)
 ASTNode *parse_redirect(Token *tokens, int token_count, int *token_index)
 {
     ASTNode *node_command = parse_atom(tokens, token_count, token_index);
-    if (!node_command) {
-        fprintf(stderr, "parse_redirect: parse_command failure\n");
+    if (!node_command)
         return NULL;
-    }
 
     ASTNode *current_node = node_command;
 
@@ -151,10 +134,8 @@ ASTNode *parse_redirect(Token *tokens, int token_count, int *token_index)
                                           tokens[*token_index].type == TOKEN_OUTPUT_REDIRECT ||
                                           tokens[*token_index].type == TOKEN_APPEND_REDIRECT)) {
         ASTNode *node = malloc(sizeof(ASTNode));
-        if (!node) {
-            fprintf(stderr, "parse_redirect: malloc failure\n");
+        if (!node)
             return NULL;
-        }
 
         node->type = NODE_REDIRECT;
         node->redirect.child = current_node;
@@ -178,10 +159,9 @@ ASTNode *parse_redirect(Token *tokens, int token_count, int *token_index)
 
         (*token_index)++;
 
-        if (tokens[*token_index].type != TOKEN_WORD) {
-            fprintf(stderr, "Invalid redirection usage\n");
+        if (tokens[*token_index].type != TOKEN_WORD)
             return NULL;
-        }
+
         node->redirect.filename = strdup(tokens[*token_index].value);
 
         (*token_index)++;
@@ -235,10 +215,8 @@ ASTNode *parse_subshell(Token *tokens, int token_count, int *token_index)
 ASTNode *parse_command(Token *tokens, int token_count, int *token_index)
 {
     ASTNode *node = malloc(sizeof(ASTNode));
-    if (!node) {
-        fprintf(stderr, "parse_command: node malloc failure");
+    if (!node)
         return NULL;
-    }
 
     node->type = NODE_COMMAND;
 
@@ -248,10 +226,8 @@ ASTNode *parse_command(Token *tokens, int token_count, int *token_index)
     }
 
     node->command.argv = malloc((word_count + 1) * sizeof(char *));
-    if (!node->command.argv) {
-        fprintf(stderr, "parse_command: command.argv malloc failure");
+    if (!node->command.argv)
         return NULL;
-    }
 
     for (int i = 0; i < word_count; i++) {
         node->command.argv[i] = strdup(tokens[*token_index + i].value);
