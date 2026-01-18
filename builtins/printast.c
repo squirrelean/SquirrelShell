bd77976207f27814ce9826dbc562acbaf9f62a1c
squirrelean <danastasio02@icloud.com>
2026-01-18 12:37:13 -0500

    Refactor: clean indentation

--- builtins/printast.c
+++ builtins/printast.c
@@ -7,12 +7,12 @@
 
 void print_tree(ASTNode *node, bool node_end, char *outline);
 void print_tree_design(int dist, bool node_end);
-char* create_new_outline(char *outline, bool node_end);
+char *create_new_outline(char *outline, bool node_end);
 
 #define BRANCH_END "└── "
 #define BRANCH_REG "├── "
-#define SPACE      "    "
-#define SEPARATOR  "|   "
+#define SPACE "    "
+#define SEPARATOR "|   "
 #define INDENT_SIZE 5
 
 void print_ast(ASTNode *ast)
@@ -22,88 +22,74 @@ void print_ast(ASTNode *ast)
     print_tree(ast, node_end, outline);
 }
 
-
 void print_tree(ASTNode *node, bool node_end, char *outline)
 {
-    if (!node)
-    {
+    if (!node) {
         printf("There is no tree to print");
         return;
     }
 
     printf("%s", outline);
-    if (node_end)
-    {
+    if (node_end) {
         printf("%s", BRANCH_END);
-    }
-    else
-    {
+    } else {
         printf("%s", BRANCH_REG);
     }
 
     char *new_outline = create_new_outline(outline, node_end);
-    if (!new_outline)
-    {
+    if (!new_outline) {
         fprintf(stderr, "print_tree: malloc failure");
         return;
     }
 
-    if (node->type == NODE_COMMAND)
-    {
+    if (node->type == NODE_COMMAND) {
         printf("COMMAND:");
-        for (int i = 0; node->command.argv[i]; i++)
-        {
+        for (int i = 0; node->command.argv[i]; i++) {
             printf(" %s", node->command.argv[i]);
         }
         printf("\n");
     }
 
-    else if (node->type == NODE_REDIRECT)
-    {
-        switch (node->redirect.redirect_type)
-        {
-            case REDIRECT_INPUT:
-                    printf("REDIRECT INPUT <");
-                    break;
-                case REDIRECT_OUTPUT:
-                    printf("REDIRECT OUTPUT >");
-                    break;
-                case REDIRECT_APPEND:
-                    printf("REDIRECT APPEND >>");
-                    break;
+    else if (node->type == NODE_REDIRECT) {
+        switch (node->redirect.redirect_type) {
+        case REDIRECT_INPUT:
+            printf("REDIRECT INPUT <");
+            break;
+        case REDIRECT_OUTPUT:
+            printf("REDIRECT OUTPUT >");
+            break;
+        case REDIRECT_APPEND:
+            printf("REDIRECT APPEND >>");
+            break;
         }
-        
+
         printf(" %s\n", node->redirect.filename);
         print_tree(node->redirect.child, true, new_outline);
     }
 
-    else if (node->type == NODE_PIPELINE ||
-             node->type == NODE_AND ||
-             node->type == NODE_OR ||
-             node->type == NODE_SEQUENCE)
-    {
-        switch (node->type)
-        {
-            case NODE_PIPELINE:
-                printf("PIPE\n");
-                break;
-            case NODE_AND:
-                printf("AND\n");
-                break;
-            case NODE_OR:
-                printf("OR\n");
-                break;
-            case NODE_SEQUENCE:
-                printf("SEQUENCE\n");
-                break;
-            default:
+    else if (node->type == NODE_PIPELINE || node->type == NODE_AND || node->type == NODE_OR ||
+             node->type == NODE_SEQUENCE) {
+        switch (node->type) {
+        case NODE_PIPELINE:
+            printf("PIPE\n");
+            break;
+        case NODE_AND:
+            printf("AND\n");
+            break;
+        case NODE_OR:
+            printf("OR\n");
+            break;
+        case NODE_SEQUENCE:
+            printf("SEQUENCE\n");
+            break;
+        default:
+            return;
         }
         print_tree(node->binary.left, false, new_outline);
         print_tree(node->binary.right, true, new_outline);
     }
 
-    else if (node->type == NODE_SUBSHELL)
-    {
+    else if (node->type == NODE_SUBSHELL) {
         printf("SUBSHELL\n");
         print_tree(node->binary.left, true, new_outline);
     }
@@ -111,26 +97,21 @@ void print_tree(ASTNode *node, bool node_end, char *outline)
     free(new_outline);
 }
 
-
-char* create_new_outline(char *outline, bool node_end)
+char *create_new_outline(char *outline, bool node_end)
 {
     size_t new_outline_size = strlen(outline) + INDENT_SIZE;
     char *new_outline = malloc(new_outline_size);
-    if (!new_outline)
-    {
+    if (!new_outline) {
         return NULL;
     }
 
     strcpy(new_outline, outline);
 
-    if (node_end)
-    {
+    if (node_end) {
         strcat(new_outline, SPACE);
-    }
-    else
-    {
+    } else {
         strcat(new_outline, SEPARATOR);
     }
-    
+
     return new_outline;
 }
