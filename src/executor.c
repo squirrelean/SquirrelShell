042868235430e63962201defc5e980e6b56a925b
squirrelean <danastasio02@icloud.com>
2026-01-20 09:28:55 -0500

    refactor: add dispatcher table for command execution, global var for cmd status

--- src/executor.c
+++ src/executor.c
@@ -7,6 +7,7 @@
 #include <sys/wait.h>
 #include <unistd.h>
 
+#include "allocation_resources.h"
 #include "builtins.h"
 #include "command_syntax_tree.h"
 
@@ -20,47 +21,47 @@ int execute_logical(ASTNode *node);
 int execute_pipeline(ASTNode *node);
 int execute_redirect(ASTNode *node);
 int execute_subshell(ASTNode *node);
-int execute_command(ASTNode *node, int last_command_status);
+int execute_command(ASTNode *node);
+int execute_dispatch(ASTNode *node);
 
-static int last_command_status = 0;
 int execute_ast(ASTNode *node)
 {
     if (!node) {
-        return 1;
+        global_command_status = 1;
+        return global_command_status;
     }
 
     switch (node->type) {
     case NODE_COMMAND:
-        last_command_status = execute_command(node, last_command_status);
+        global_command_status = execute_dispatch(node);
         break;
 
     case NODE_SEQUENCE:
-        last_command_status = execute_sequence(node);
+        global_command_status = execute_sequence(node);
         break;
 
     case NODE_PIPELINE:
-        last_command_status = execute_pipeline(node);
+        global_command_status = execute_pipeline(node);
         break;
 
     case NODE_REDIRECT:
-        last_command_status = execute_redirect(node);
+        global_command_status = execute_redirect(node);
         break;
 
     case NODE_AND:
     case NODE_OR:
-        last_command_status = execute_logical(node);
+        global_command_status = execute_logical(node);
         break;
 
     case NODE_SUBSHELL:
-        last_command_status = execute_subshell(node);
+        global_command_status = execute_subshell(node);
         break;
 
     default:
         fprintf(stderr, "SquirrelShell: invalid node type\n");
-        last_command_status = 1;
+        global_command_status = 1;
     }
-
-    return last_command_status;
+    return global_command_status;
 }
 
 int execute_sequence(ASTNode *node)
@@ -203,30 +204,36 @@ int execute_subshell(ASTNode *node)
     }
 }
 
-int execute_command(ASTNode *node, int last_command_status)
+int execute_dispatch(ASTNode *node)
 {
     char *command = node->command.argv[0];
 
     if (strcmp(command, EXIT_SHELL) == 0)
-        exit_shell(last_command_status);
+        exit_shell();
 
     else if (strcmp(command, CHANGE_DIRECTORY) == 0)
         change_directory(node->command.argv);
 
-    else {
-        pid_t pid = fork();
-        if (pid == -1)
-            return 1;
-        else if (!pid) {
-            execvp(node->command.argv[0], node->command.argv);
-            exit(1);
-        } else {
-            int status;
-            waitpid(pid, &status, 0);
-            int status_code = WEXITSTATUS(status);
-            return status_code;
-        }
-    }
+    else if (strcmp(command, PRINT_AST) == 0)
+        print_ast(node->command.argv[1]);
+    else
+        return execute_command(node);
 
     return 0;
+}
+
+int execute_command(ASTNode *node)
+{
+    pid_t pid = fork();
+    if (pid == -1)
+        return 1;
+    else if (!pid) {
+        execvp(node->command.argv[0], node->command.argv);
+        exit(1);
+    } else {
+        int status;
+        waitpid(pid, &status, 0);
+        int status_code = WEXITSTATUS(status);
+        return status_code;
+    }
 }
