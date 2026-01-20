2e2123d0a623328ce407ee87aaea157ad15079c2
squirrelean <danastasio02@icloud.com>
2026-01-20 09:33:46 -0500

    Refactor: use global status var for exit code

--- builtins/exit.c
+++ builtins/exit.c
@@ -1,35 +1,32 @@
-#include <stdlib.h>
-#include "tokenizer.h"
-#include "parser.h"
 #include "allocation_resources.h"
+#include "parser.h"
+#include "tokenizer.h"
+#include <stdlib.h>
 
 #include <stdio.h>
 
 void free_all_allocated_memory();
 
-void exit_shell(int exit_status)
+void exit_shell()
 {
     free_all_allocated_memory();
 
-    printf("SquirrelShell: Exiting shell: Last Executed Command Status: %d\n", exit_status);
-    
-    exit(exit_status);
+    printf("SquirrelShell: Exiting shell: Last Executed Command Status: %d\n", global_command_status);
+
+    exit(global_command_status);
 }
 
 void free_all_allocated_memory()
 {
-    if (global_allocated_line)
-    {
+    if (global_allocated_line) {
         free(global_allocated_line);
     }
 
-    if (global_allocated_tokens)
-    {
+    if (global_allocated_tokens) {
         free_tokens(global_allocated_tokens, global_allocated_token_count);
     }
 
-    if (global_allocated_ast)
-    {
+    if (global_allocated_ast) {
         free_ast(global_allocated_ast);
     }
 }
