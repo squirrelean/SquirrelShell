d702247633387f321aa6e8e9e1818afe4f3ae415
squirrelean <danastasio02@icloud.com>
2026-01-20 09:25:18 -0500

    add global varaible for last exec command status

--- include/allocation_resources.h
+++ include/allocation_resources.h
@@ -1,11 +1,12 @@
 #ifndef ALLOCATION_RESOURCES_H
 #define ALLOCATION_RESOURCES_H
-#include "tokenizer.h"
 #include "command_syntax_tree.h"
+#include "tokenizer.h"
 
 extern char *global_allocated_line;
 extern Token *global_allocated_tokens;
 extern int global_allocated_token_count;
 extern ASTNode *global_allocated_ast;
+extern int global_command_status;
 
 #endif
